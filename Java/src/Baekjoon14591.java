8ebc92577723aa37e81fb2df23fa8de4ad68b670
김남우 <treenicole@naver.com>
2024-03-01 20:31:58 +0900

    14581_팬들에게_둘러싸인_홍준.java

---
+++ Java/src/Baekjoon14591.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Baekjoon14591 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::"  + str + "::fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+}
