4f15ab01ab28157ed86f64ec0777c6addb68a909
김남우 <treenicole@naver.com>
2024-04-04 20:53:33 +0900

    15650_N과_M(2).py

---
+++ Python/15650_N과_M(2).py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+def backtracking():
+    if len(answer) == m:
+        print(" ".join(map(str, answer)))
+    else:
+        for i in range(1, n+1):
+            if len(answer) == 0:
+                backtracking_loop(i)
+            elif i not in answer and answer[-1] < i:
+                backtracking_loop(i)
+
+def backtracking_loop(i):
+    answer.append(i)
+    backtracking()
+    answer.pop()
+
+n, m = map(int, input().split())
+answer = []
+
+backtracking()
