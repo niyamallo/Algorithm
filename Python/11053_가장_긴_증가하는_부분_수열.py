7a6739a3a737555dac846ae1ba8988a1621a915a
김남우 <treenicole@naver.com>
2024-04-07 16:22:07 +0900

    11053_가장_긴_증가하는_부분_수열.py

---
+++ Python/11053_가장_긴_증가하는_부분_수열.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+nums = list(map(int, input().split()))
+dp = [0]*n
+
+for i in range(n):
+    for j in range(i):
+        if nums[i] > nums[j] and dp[i] < dp[j]:
+            dp[i] = dp[j]
+    dp[i] += 1
+
+print(max(dp))
