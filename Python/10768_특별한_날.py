339a484b31fbe51d63c637ae391c61efaf8fd4c9
김남우 <treenicole@naver.com>
2024-05-11 21:43:26 +0900

    10768_특별한_날.py

---
+++ Python/10768_특별한_날.py
@@ -0,0 +1,11 @@
+m = int(input())
+d = int(input())
+
+if m > 2:
+    print("After")
+elif m == 2 and d>18:
+    print("After")
+elif m == 2 and d == 18:
+    print("Special")
+else:
+    print("Before")
