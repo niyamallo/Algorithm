5600afe91a102ebfc6b404164362edc464808859
김남우 <treenicole@naver.com>
2024-06-13 20:03:03 +0900

    1476_날짜_계산.py

---
+++ Python/1476_날짜_계산.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+e, s, m = map(int, input().split())
+year = 1
+
+while True:
+    if ((year - e) % 15 == 0) and ((year - s) % 28 == 0) and ((year - m) % 19 == 0):
+        print(year)
+        break
+    else:
+        year += 1
