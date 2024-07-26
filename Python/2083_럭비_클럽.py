82a3347a32808a946c1b968bf3cc30857fe7d453
김남우 <treenicole@naver.com>
2024-07-27 01:25:51 +0900

    2083_럭비_클럽.py

---
+++ Python/2083_럭비_클럽.py
@@ -0,0 +1,10 @@
+import sys; input = sys.stdin.readline
+
+while True:
+    name, age, weight = input().split()
+    if name == "#":
+        break
+    if int(age) > 17 or int(weight) >= 80:
+        print(name, "Senior")
+    else:
+        print(name, "Junior")
